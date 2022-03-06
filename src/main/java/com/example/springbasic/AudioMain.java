2dba5c04cb46a1c81db68a4a24af72d86e6dc6c2
dqshen <dqshen@hogwarts>
2022-03-06 17:00:53 +0800

    用xml声明bean,bean声明的基本模式

--- src/main/java/com/example/springbasic/AudioMain.java
+++ src/main/java/com/example/springbasic/AudioMain.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AudioMain {
     public static void main(String[] args) {
+        // 用最基础的类资源加载方式初始化spring上下文
         ClassPathXmlApplicationContext context =
         new ClassPathXmlApplicationContext("META-INF/spring/ApplicationContext.xml");
         CompactDisc disc = context.getBean(CompactDisc.class);
