ddd58d361ee269d22239f5b680717a15e74a1ca6
dqshen <dqshen@hogwarts>
2022-03-13 15:58:26 +0800

    通过system-lamda包对自动装配结果进行断言测试

--- src/main/java/com/example/springbasic/SgtPeppers.java
+++ src/main/java/com/example/springbasic/SgtPeppers.java
@@ -7,7 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class SgtPeppers implements CompactDisc {
-    public void play(){
-        log.info("SgtPeppers\' playing!");
+
+    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+    private String artist = "The Beatles";
+
+    public void play() {
+        log.info("Playing " + title + " by " + artist);
     }
 }
--- src/test/java/com/example/springbasic/CDplayerTest.java
+++ src/test/java/com/example/springbasic/CDplayerTest.java
@@ -1,6 +1,9 @@
 package com.example.springbasic;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
  * @SpringBootTest是spring-boot提供的组合注解，
  * 包括重要的@ExtendWith(value=SpringExtension.class),
  * 它是确定测试类里的断言、测试等等注解将如何执行的测试类常用注解
+ * 
  * @ContextConfiguration引入名为CDPlayerConfig的bean配置类
  */
 @SpringBootTest
@@ -19,9 +23,23 @@ public class CDplayerTest {
 
     @Autowired
     private CompactDisc cd;
-    
+
+    @Autowired
+    private MediaPlayer player;
+
     @Test
-    public void cdShouldNotBeNull(){
+    public void cdShouldNotBeNull() {
         assertNotNull(cd);
     }
+
+    @Test
+    public void play() throws Exception {
+        /* 引入system-lamda包截取系统输出，进行断言测试 */
+        String text = tapSystemOut(() -> {
+            player.play();
+        });
+
+        assertTrue(text.endsWith(
+                "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n"));
+    };
 }
